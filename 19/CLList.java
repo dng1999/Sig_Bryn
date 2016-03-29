53c19db7c22920bf005edb642d0f3d3439b21fee
Dorothy Ng <dng1@stuy.edu>
2016-03-28 21:38:28 -0400

    floor complete

--- 19/CLList.java
+++ 19/CLList.java
@@ -1,3 +1,9 @@
+/*
+Dorothy Ng
+APCS2 pd10
+HW19 -- 
+2016-03-28
+ */
 /*****************************************************
  * skeleton for class CLList 
  * implements a circularly-linked list of doubly-linked nodes
@@ -10,33 +16,65 @@ public class CLList<T> {
     private DLLNode<T> _head;
     private int _size = 0;
 
-    public CLList() 
-	{
+    public CLList() {
+	_head = null;
     }
 
+    //returns size of c-linked list
     public int size() { return _size; }
-
-    public boolean add( T x ) 
-    {
+    
+    //adds node to end of c-linked list
+    public boolean add( T x ) {
+	if (_size == 0) {
+	    _head = new DLLNode(x,null,null);
+	    _head.setPrev(_head);
+	    _head.setNext(_head);
+	}
+	else if (_size == 1){
+	    DLLNode<T> tmp = new DLLNode(x,_head,_head);
+	    _head.setPrev(tmp);
+	    _head.setNext(tmp);
+	}
+	else {
+	    DLLNode<T> tmp = new DLLNode(x,_head.getPrev(),_head);
+	    _head.getPrev().setNext(tmp);
+	    _head.setPrev(tmp);
+	}
+	_size++;
 	return true;
     }
-
-
-    public T get( int index ) 
-    {
-
+    
+    //returns value of node at index
+    public T get( int index ) {
+	DLLNode<T> tmp = _head;
+	for (int i=0;i<index;i++){
+	    tmp = tmp.getNext();
+	}
+	return tmp.getCargo();
     }
 
-
-    public T set( int index, T newVal ) 
-    {
-
+    //replaces value of node at index
+    //returns replaced value
+    public T set( int index, T newVal ) {
+	DLLNode<T> tmp = _head;	
+	for (int i=0;i<index;i++){
+	    tmp = tmp.getNext();
+	}
+	T hold = tmp.getCargo();
+	tmp.setCargo(newVal);
+	return hold;
     }
 
-
-    public String toString() 
-    {
-
+    //returns String of c-linked list
+    public String toString() {
+	String ret = _head.getCargo()+" -> ";
+	DLLNode<T> tmp = _head.getNext();
+	while (tmp != _head){
+	    ret += tmp.getCargo()+" -> ";
+	    tmp = tmp.getNext();
+	}
+	ret += "NULL";
+	return ret;
     }
 
 

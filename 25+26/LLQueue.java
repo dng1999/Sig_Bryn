d92df26ccdaf4e0ac0cc4221ec81211bc4bd7f49
yureistar <dng1@stuy.edu>
2016-04-04 15:35:15 -0400

    lol

--- 25+26/LLQueue.java
+++ 25+26/LLQueue.java
@@ -15,46 +15,48 @@
 public class LLQueue<T> implements Queue<T> {
 
     private LLNode<T> _front, _end;
-
+        
     // default constructor creates an empty queue
-    public LLQueue()
- { 
-
+    public LLQueue() { 
+	_front = new LLNode<T>(null,null);
+	_end = new LLNode<T>(null,_front);
     }
-
+    
     // means of adding a thing to the collection
-    public void enqueue( T enQVal ) 
-{
-
-    }//O(?)
-
-
+    public void enqueue( T enQVal ) {
+	LLNode<T> add = new LLNode<T>(enQVal,_end);
+	_end = add;
+    }//O(1)
+    
+    
     // means of removing a thing from the collection
     // remove and return thing at front of queue
     // assume _queue ! empty
-    public T dequeue() 
-{ 
-
-    }//O(?)
+    public T dequeue() { 
+	LLNode<T> stor = _front;
+	LLNode<T> ctr = _end;
+	for (int i = 0; i < _size; i++){
+	    ctr = ctr.getNext();
+	    if (ctr.getNext().equals(_front)) _front = ctr; 
+	}
+	return stor.getValue();
+    }//O(n)
 
 
     // means of peeking at thing next in line for removal
-    public T peekFront() 
-{
-
+    public T peekFront() {
+	return _front.getValue();
     }//O(?)
 
 
-    public boolean isEmpty() 
-{
-
+    public boolean isEmpty() {
+	return false;
     }//O(?)
 
 
     // print each node, separated by spaces
-    public String toString() 
-{ 
-
+    public String toString() { 
+	return "";
     }//O(?)
 
 

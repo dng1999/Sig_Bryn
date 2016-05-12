c9dd7e97e960d5d371a73e9b6ec932294397af70
yureistar <dng1@stuy.edu>
2016-05-12 16:51:48 -0400

    rakue

---
+++ 38/TreeNode.java
@@ -0,0 +1,92 @@
+/*****************************************************
+ * class TreeNode
+ * Implements a node for use in a binary tree.
+ * Only holds int cargo.
+ *****************************************************/
+
+public class TreeNode{
+
+    //instance variables / attributes of a TreeNode:
+    private TreeNode left;
+    private TreeNode right;
+    private int value;
+
+    /*****************************************************
+     * default constructor
+     * Construct a tree node with specified value, 
+     * with null left and right subtrees.
+     *****************************************************/
+    public TreeNode( int initValue )
+    {	
+	value = initValue;
+	left = null;
+	right = null;
+    }
+          
+
+    /*****************************************************
+     * overloaded constructor
+     * Construct a tree node with specified value, left, and right subtrees.
+     *****************************************************/
+    public TreeNode( int initValue, TreeNode initLeft, TreeNode initRight ) 
+    {
+	value = initValue;
+	left = initLeft;
+	right = initRight;
+    }
+ 
+
+    /*****************************************************
+     * Returns the left subtree of this node.
+     *****************************************************/
+    public TreeNode getLeft() 
+    {
+	return left;
+    }
+
+
+    /*****************************************************
+     * Returns the right subtree of this node.
+     *****************************************************/
+    public TreeNode getRight() 
+    {
+	return right;
+    }
+
+
+    /*****************************************************
+     * Returns the value stored in this tree node.
+     *****************************************************/
+    public int getValue() 
+    {
+	return value;
+    }
+
+
+    /*****************************************************
+     * Sets the value of the left subtree of this node.
+     *****************************************************/
+    public void setLeft( TreeNode theNewLeft ) 
+    {
+	left = theNewLeft;
+    }
+
+
+    /*****************************************************
+     * Sets the value of the right subtree of this node.
+     *****************************************************/
+    public void setRight( TreeNode theNewRight ) 
+    {
+	right = theNewRight;
+    }
+
+
+    /*****************************************************
+     * Sets the value of this tree node.
+     *****************************************************/
+    void setValue( int theNewValue ) 
+    {
+	value = theNewValue;
+    }
+ 
+}//end class
